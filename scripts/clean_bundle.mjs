90e2bafee50f95ae34508df18e9d3f89e6adea3a
Mark <mark.afonin01@mail.ru>
2026-09-16 20:36:29 +0300

    Replace old flight site with polished continuous journey

--- scripts/clean_bundle.mjs
+++
@@ -1,3 +0,0 @@
-import fs from 'node:fs';
-const file = new URL('../scene.js', import.meta.url);
-fs.writeFileSync(file, fs.readFileSync(file, 'utf8').replace(/[ \t]+$/gm, '').replace(/^ +\t/gm, '\t'));
